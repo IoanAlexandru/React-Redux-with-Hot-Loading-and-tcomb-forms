34851be2428dfac55f2f4eca50b12ee5bbcd14be
Alex Ioan <email@example.com>
2015-10-06 16:37:02 +0300

    Added simple authentication to admin route

--- app.js
+++ app.js
@@ -17,14 +17,27 @@ app.set('view engine', 'jade');
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!user.verifyPassword(password)) { return done(null, false); }
-      return done(null, user);
-    });
+    // User.findOne({ username: username }, function (err, user) {
+    //   if (err) { return done(err); }
+    //   if (!user) { return done(null, false); }
+    //   if (!user.verifyPassword(password)) { return done(null, false); }
+    //   return done(null, user);
+    // });
+    if(username == "johndoe" && password == "password"){
+      done(null, { id: 123, name: username})
+    } else {
+      done(null, false)
+    }
   }
 ));
+
+passport.serializeUser(function(user, done){
+  done(null, user.id)
+})
+
+passport.deserializeUser(function(id, done){
+  done(null, { id: id, name: id})
+})
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -39,7 +52,7 @@ app.use(passport.session());
 
 app.use('/', routes);
 app.use('/registration', express.static(path.join(__dirname, 'app-registration')));
-app.use('/admin', express.static(path.join(__dirname, 'app-admin')));
+app.use('/admin', ensureAuthenticated, express.static(path.join(__dirname, 'app-admin')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -72,5 +85,13 @@ app.use(function(err, req, res, next) {
   });
 });
 
+
+function ensureAuthenticated(req, res, next){
+  if(req.isAuthenticated()){
+    next()
+  } else {
+    res.redirect('/login')
+  }
+}
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport')
+var passport = require('passport');
 
 var persons = [{
     name: "Ioan",
@@ -17,7 +17,7 @@ var persons = [{
   }];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/' , function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
@@ -36,9 +36,9 @@ router.get('/logout', function(req, res){
 });
 
 router.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login',
-                                   failureFlash: true })
+  passport.authenticate('local'), function(req,res,next){
+    res.redirect('/admin')
+  }
 );
 
 router.get('/persons', function(req, res, next){
